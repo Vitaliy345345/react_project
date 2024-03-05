d915195cac0d6c8d9f920eaf03c89ad72e20f3b3
Vitaliy Sereda <1239874560ret@gmail.com>
2024-03-05 19:46:33 +0200

    Added new pages files

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './store';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
+import './styles/index.css';
+import { Paths } from './paths';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const router = createBrowserRouter([
+  {
+    path: Paths.home,
+    element: <App/>
+  },
+  {
+    path: Paths.login,
+    element: <h1>Login</h1>
+  },
+  {
+    path: Paths.register,
+    element: <h1>Register</h1>
+  },
+  {
+    path: Paths.user,
+    element: <h1>User</h1>
+  },
+  {
+    path: Paths.settings,
+    element: <h1>Settings</h1>
+  },
+])
+
 root.render(
   <Provider store={store}>
-    <App />
+    <RouterProvider router={router}/>
   </Provider>
 );
 reportWebVitals();
